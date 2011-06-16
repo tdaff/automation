4ba135308df652a4684ac38bf4e6dd19c1f0247a
Tom Daff <tdd20@cam.ac.uk>
2011-06-16 16:57:42 -0400

    setting up framework TODO: program design!

--- help.py
+++ help.py
@@ -6,32 +6,47 @@ PyTurds
 Automated high throughput strucutre analysis
 """
 
-def get_opts():
-    """Define usage options, some error checking"""
-    usage = "usage: %prog [options] INFILE FX FY FZ OUTPREFIX"
-    parser = OptionParser(usage=usage, version="%prog 0.1",
-                          description=__doc__)
-    parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
-                      help="write extra information to stdout [default]",
-                      default=True)
-    parser.add_option("-q", "--quiet", action="store_false", dest="verbose",
-                      help="silence all output")
-    parser.add_option("-c", "--crop", action="store_true", dest="crop",
-                      help="crop atoms to those in the cell [default]",
-                      default=True)
-    parser.add_option("-n", "--no-crop", action="store_false", dest="crop",
-                      help="do not crop atoms; use original configuration")
-    parser.add_option("-z", "--zoom", dest="zoom", type='float',
-                      help="resize the grid with a spline interpolation")
-    parser.add_option("-A", action="store_true", dest="flatten_a",
-                      help="flatten along the a axis")
-    parser.add_option("-B", action="store_true", dest="flatten_b",
-                      help="flatten along the b axis")
-    parser.add_option("-C", action="store_true", dest="flatten_c",
-                      help="flatten along the c axis")
-    (local_options, local_args) = parser.parse_args()
+from optparse import OptionParser
+import pickle
 
-    if len(local_args) != 5:
-        parser.error("Five arguments are required (try %prog --help)")
 
-    return (local_options, local_args)
+class Options(object):
+    """
+    A single object to deal with input files and command line options but
+    delivering reasonable defaults for unspecified values.
+    """
+    def __init__(self):
+        self._commandline()
+
+    def _commandline(self):
+        """Define usage options, some error checking"""
+        usage = "usage: %prog [options] COMMAND"
+        parser = OptionParser(usage=usage, version="%prog 0.1",
+                              description=__doc__)
+        parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
+                          help="write extra information to stdout [default]",
+                          default=True)
+        parser.add_option("-q", "--quiet", action="store_false", dest="verbose",
+                          help="silence all output")
+        (local_options, local_args) = parser.parse_args()
+
+        #if len(local_args) != 1:
+        #    parser.error("No command given (try %prog --help)")
+
+        self.options = local_options
+        self.args = local_args
+
+class Simulation(object):
+    """
+    A single set of calculations for a structure
+    """
+    def __init__(self, options):
+        self.options = options
+
+
+
+
+if __name__ == '__main__':
+    global_options = Options()
+    my_simulation = Simulation(global_options)
+    print(pickle.dumps(my_simulation))

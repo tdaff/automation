a1abfb9a5086e7c7ac6f31d40a0b51633e781f91
Tom Daff <tdd20@cam.ac.uk>
2013-03-05 12:07:37 -0500

    Fixed typo for parital/partial charges; should read old files for now.
    closes #51

--- faps.py
+++ faps.py
@@ -2976,7 +2976,9 @@ class Atom(object):
             self.uff_type = at_dict['_atom_site_description']
         elif '_atom_type_description' in at_dict:
             self.uff_type = at_dict['_atom_type_description']
-        if '_atom_type_parital_charge' in at_dict:
+        if '_atom_type_partial_charge' in at_dict:
+            self.charge = float(at_dict['_atom_type_partial_charge'])
+        elif '_atom_type_parital_charge' in at_dict:
             self.charge = float(at_dict['_atom_type_parital_charge'])
 
     def from_pdb(self, line, charges=False):

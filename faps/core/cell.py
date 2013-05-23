a2b30e62e51776c99a37b3eb39e01298d15f3be4
Tom Daff <tdd20@cam.ac.uk>
2013-05-23 17:26:14 -0400

    more restructuring

---
+++ faps/core/cell.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python
+
+"""
+Cell object for use in periodic structures and interconversions between
+matrix and parameter representations.
+
+"""
+
+from itertools import count
+from logging import warning, debug, error, info, critical
+from math import ceil
+
+import numpy as np
+from numpy import pi, radians, cos, sin, sqrt, arccos
+from numpy import array, identity, dot, cross
+from numpy.linalg import norm
+
+
+class Cell(object):
+    """
+    Crystollagraphic cell representations and interconversion methods.
+
+    Setter methods can be defined for different file types, however
+    .cell and .params will be self-consistent if set directly.
+
+    """
+
+    def __init__(self, cell=None, params=None):
+        """Default to a 1A cubic box."""
+        if cell is not None:
+            self.cell = cell
+        elif params is not None:
+            self.params = params
+        else:
+            self.cell = [1.0, 0.0, 0.0,
+                         0.0, 1.0, 0.0,
+                         0.0, 0.0, 1.0]
+
+    def from_pdb(self, line):
+        """Extract cell from CRYST1 line in a pdb."""
+        # Must use fixed widths as -ve numbers do not leave gaps to .split()
+        self.params = (float(line[6:15]),
+                       float(line[15:24]),
+                       float(line[24:33]),
+                       float(line[33:40]),
+                       float(line[40:47]),
+                       float(line[47:54]))
+
+    def from_lines(self, lines, scale=1.0):
+        """Extract cell from a 3-line POSCAR cell representation."""
+        self.cell = array([[float(x) * scale for x in lines[0].split()],
+                           [float(x) * scale for x in lines[1].split()],
+                           [float(x) * scale for x in lines[2].split()]])
+
+    def to_vector_strings(self, scale=1, bohr=False, fmt="%20.12f"):
+        """Generic [Super]cell vectors in Angstrom as a list of strings."""
+        out_format = 3 * fmt + "\n"
+        # If the supercell is more than 2147483647 in any direction this
+        # will fail in python 2, but 'long' removed for py3k forward
+        # compatibility
+        if isinstance(scale, int):
+            scale = [scale, scale, scale]
+            # else assume an iterable 3-vector
+        if bohr:
+            scale = [x / BOHR2ANG for x in scale]
+        return [out_format % tuple(scale[0] * self.cell[0]),
+                out_format % tuple(scale[1] * self.cell[1]),
+                out_format % tuple(scale[2] * self.cell[2])]
+
+    def minimum_supercell(self, cutoff):
+        """Calculate the smallest supercell with a half-cell width cutoff."""
+        a_cross_b = cross(self.cell[0], self.cell[1])
+        b_cross_c = cross(self.cell[1], self.cell[2])
+        c_cross_a = cross(self.cell[2], self.cell[0])
+
+        volume = dot(self.cell[0], b_cross_c)
+
+        widths = [volume / norm(b_cross_c),
+                  volume / norm(c_cross_a),
+                  volume / norm(a_cross_b)]
+
+        return tuple(int(ceil(2*cutoff/x)) for x in widths)
+
+    @property
+    def imcon(self):
+        """Guess cell shape and return DL_POLY imcon key."""
+        keys = {'none': 0,
+                'cubic': 1,
+                'orthorhombic': 2,
+                'parallelepiped': 3}
+        if np.all(self.cell == 0):
+            return keys['none']
+        elif np.all(self.params[3:] == 90):
+            if self.params[0] == self.params[1] == self.params[2]:
+                return keys['cubic']
+            else:
+                return keys['orthorhombic']
+        else:
+            return keys['parallelepiped']
+
+    @property
+    def crystal_system(self):
+        """Return the IUCr designation for the crystal system."""
+        #FIXME(tdaff): must be aligned with x to work
+        if self.alpha == self.beta == self.gamma == 90:
+            if self.a == self.b == self.c:
+                return 'cubic'
+            elif self.a == self.b or self.a == self.c or self.b == self.c:
+                return 'tetragonal'
+            else:
+                return 'orthorhombic'
+        elif self.alpha == self.beta == 90:
+            if self.a == self.b and self.gamma == 120:
+                return 'hexagonal'
+            else:
+                return 'monoclinic'
+        elif self.alpha == self.gamma == 90:
+            if self.a == self.c and self.beta == 120:
+                return 'hexagonal'
+            else:
+                return 'monoclinic'
+        elif self.beta == self.gamma == 90:
+            if self.b == self.c and self.alpha == 120:
+                return 'hexagonal'
+            else:
+                return 'monoclinic'
+        elif self.a == self.b == self.c and self.alpha == self.beta == self.gamma:
+            return 'trigonal'
+        else:
+            return 'triclinic'
+
+    @property
+    def volume(self):
+        """Calculate cell volume a.bxc."""
+        b_cross_c = cross(self.cell[1], self.cell[2])
+        return dot(self.cell[0], b_cross_c)
+
+    def get_cell(self):
+        """Get the 3x3 vector cell representation."""
+        return self._cell
+
+    def set_cell(self, value):
+        """Set cell and params from the cell representation."""
+        # Class internally expects an array
+        self._cell = array(value).reshape((3,3))
+        self.__mkparam()
+        self._inverse = np.linalg.inv(self.cell.T)
+
+    # Property so that params are updated when cell is set
+    cell = property(get_cell, set_cell)
+
+    def get_params(self):
+        """Get the six parameter cell representation as a tuple."""
+        return tuple(self._params)
+
+    def set_params(self, value):
+        """Set cell and params from the cell parameters."""
+        self._params = value
+        self.__mkcell()
+        self._inverse = np.linalg.inv(self.cell.T)
+
+    # Property so that cell is updated when params are set
+    params = property(get_params, set_params)
+
+    @property
+    def inverse(self):
+        """Inverted cell matrix for converting to fractional coordinates."""
+#        try:
+#            if self._inverse is None:
+#                self._inverse = np.linalg.inv(self.cell.T)
+#        except AttributeError:
+#            self._inverse = np.linalg.inv(self.cell.T)
+        return self._inverse
+
+    @property
+    def a(self):
+        """Magnitude of cell a vector."""
+        return self.params[0]
+
+    @property
+    def b(self):
+        """Magnitude of cell b vector."""
+        return self.params[1]
+
+    @property
+    def c(self):
+        """Magnitude of cell c vector."""
+        return self.params[2]
+
+    @property
+    def alpha(self):
+        """Cell angle alpha."""
+        return self.params[3]
+
+    @property
+    def beta(self):
+        """Cell angle beta."""
+        return self.params[4]
+
+    @property
+    def gamma(self):
+        """Cell angle gamma."""
+        return self.params[5]
+
+    # Implementation details -- directly access the private _{cell|param}
+    # attributes; please don't break.
+    def __mkcell(self):
+        """Update the cell representation to match the parameters."""
+        a_mag, b_mag, c_mag = self.params[:3]
+        alpha, beta, gamma = [radians(x) for x in self.params[3:]]
+        a_vec = array([a_mag, 0.0, 0.0])
+        b_vec = array([b_mag * cos(gamma), b_mag * sin(gamma), 0.0])
+        c_x = c_mag * cos(beta)
+        c_y = c_mag * (cos(alpha) - cos(gamma) * cos(beta)) / sin(gamma)
+        c_vec = array([c_x, c_y, (c_mag**2 - c_x**2 - c_y**2)**0.5])
+        self._cell = array([a_vec, b_vec, c_vec])
+
+    def __mkparam(self):
+        """Update the parameters to match the cell."""
+        cell_a = sqrt(sum(x**2 for x in self.cell[0]))
+        cell_b = sqrt(sum(x**2 for x in self.cell[1]))
+        cell_c = sqrt(sum(x**2 for x in self.cell[2]))
+        alpha = arccos(sum(self.cell[1, :] * self.cell[2, :]) /
+                       (cell_b * cell_c)) * 180 / pi
+        beta = arccos(sum(self.cell[0, :] * self.cell[2, :]) /
+                      (cell_a * cell_c)) * 180 / pi
+        gamma = arccos(sum(self.cell[0, :] * self.cell[1, :]) /
+                       (cell_a * cell_b)) * 180 / pi
+        self._params = (cell_a, cell_b, cell_c, alpha, beta, gamma)
+
+    @property
+    def to_dict(self):
+        drepr = {"@module": self.__class__.__module__,
+                 "@class": self.__class__.__name__,
+                 "cell": self.cell.tolist()}
+        return drepr
+
+    @staticmethod
+    def from_dict(drepr):
+        return Cell(cell=drepr['cell'])
+

a94a38772646af62a0526a44859df62406643dfc
Tom Daff <tdd20@cam.ac.uk>
2013-05-23 17:29:14 -0400

    more restructuring

---
+++ faps/chemistry/constants.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""
+Physical constants for chemistry.
+
+
+"""
+
+from numpy import pi
+
+# Global constants
+DEG2RAD = pi / 180.0
+BOHR2ANG = 0.52917720859
+EV2KCAL = 23.060542301389
+NAVOGADRO = 6.02214129E23
+INFINITY = float('inf')
+KCAL_TO_KJ = 4.1868  # Steam tables from openbabel
---
+++ tests/test_cell.py
@@ -0,0 +1,64 @@
+#!usr/bin/env python
+
+"""
+Testing Cell functionality
+
+"""
+
+import sys
+import unittest
+
+# This is where faps gets imported
+sys.path.append('..')
+from faps.core.cell import Cell
+
+class TestInitialiseCell(unittest.TestCase):
+    def test(self):
+        new_cell = Cell()
+        self.assertEqual(new_cell.cell.tolist(),
+                         [[1.0, 0.0, 0.0],
+                          [0.0, 1.0, 0.0],
+                          [0.0, 0.0, 1.0]])
+        self.assertEqual(new_cell.params, (1.0, 1.0, 1.0, 90, 90, 90))
+
+
+class TestCellCrystalSystems(unittest.TestCase):
+
+    def setUp(self):
+        # cell to manipulate
+        self.cell = Cell()
+
+    def test_crystal_perception(self):
+        # Make sure that DL_POLY imcons work
+        self.cell.params = (10, 10, 10, 90, 90, 90)
+        self.assertEqual(self.cell.crystal_system, 'cubic')
+        self.cell.params = (10, 10, 12, 90, 90, 90)
+        self.assertEqual(self.cell.crystal_system, 'tetragonal')
+        self.cell.params = (10, 11, 10, 90, 90, 90)
+        self.assertEqual(self.cell.crystal_system, 'tetragonal')
+        self.cell.params = (13, 10, 10, 90, 90, 90)
+        self.assertEqual(self.cell.crystal_system, 'tetragonal')
+        self.cell.params = (10, 11, 12, 90, 90, 90)
+        self.assertEqual(self.cell.crystal_system, 'orthorhombic')
+        self.cell.params = (10, 11, 12, 90, 70, 90)
+        self.assertEqual(self.cell.crystal_system, 'monoclinic')
+        self.cell.params = (10, 11, 12, 90, 90, 70)
+        self.assertEqual(self.cell.crystal_system, 'monoclinic')
+        self.cell.params = (10, 11, 12, 70, 90, 90)
+        self.assertEqual(self.cell.crystal_system, 'monoclinic')
+        self.cell.params = (10, 11, 12, 60, 70, 80)
+        self.assertEqual(self.cell.crystal_system, 'triclinic')
+        self.cell.params = (10, 10, 12, 90, 90, 120)
+        self.assertEqual(self.cell.crystal_system, 'hexagonal')
+        self.cell.params = (10, 11, 10, 90, 120, 90)
+        self.assertEqual(self.cell.crystal_system, 'hexagonal')
+        self.cell.params = (13, 10, 10, 120, 90, 90)
+        self.assertEqual(self.cell.crystal_system, 'hexagonal')
+        self.cell.params = (10, 10, 10, 67, 67, 67)
+        self.assertEqual(self.cell.crystal_system, 'trigonal')
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=3)
